50af9a2e42c0617ed2c1ae0122e4efdbdce9081f
mariamtkemaladze <mariamtkemaladze1234@gmail.com>
2025-01-03 23:28:11 +0400

    second commit

---
+++ level 23/home/homework.py
@@ -0,0 +1,9 @@
+name = input("enter your name:")
+last_name= input("enter your last name:")
+age = input(" enter your age:")
+favourite_place= input("enter your favorite place:")
+favourite_car= input("enter your favourite car:")
+favourite_hobby= input("enter your favourite hobby:")
+favourite_colour= input("enter your favorite colour:")
+
+print("hello, my name is",  name, "and my surname is", last_name ,"." "my favourite place is", favourite_place, "and my favourite car is", favourite_car, "." "my hobby is", favourite_hobby, "and i love", favourite_colour,"." )

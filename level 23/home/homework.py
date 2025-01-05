b067ca216e69a1123fae5ac2107d592c6e227cad
mariamtkemaladze <mariamtkemaladze1234@gmail.com>
2025-01-05 20:47:37 +0400

    second commit

--- level 23/home/homework.py
+++ level 23/home/homework.py
@@ -6,4 +6,7 @@ favourite_car= input("enter your favourite car:")
 favourite_hobby= input("enter your favourite hobby:")
 favourite_colour= input("enter your favorite colour:")
 
-print("hello, my name is",  name, "and my surname is", last_name ,"." "my favourite place is", favourite_place, "and my favourite car is", favourite_car, "." "my hobby is", favourite_hobby, "and i love", favourite_colour,"." )
+def aboutme():
+    print("hello, my name is",  name, "and my surname is", last_name ,"." "my favourite place is", favourite_place, "and my favourite car is", favourite_car, "." "my hobby is", favourite_hobby, "and i love", favourite_colour,"." )
+
+aboutme()
